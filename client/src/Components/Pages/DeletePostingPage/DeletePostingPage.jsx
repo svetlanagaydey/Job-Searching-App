4dc5248778cb99219bb2278e7d3f511f2b45a896
svetlanagaydey <lana84@ukr.net>
2022-02-10 22:20:30 +0200

    deleting posting page styles refactor completed

--- client/src/Components/Pages/DeletePostingPage/DeletePostingPage.jsx
+++ client/src/Components/Pages/DeletePostingPage/DeletePostingPage.jsx
@@ -36,54 +36,43 @@ const DeletePostingPage = () => {
     const printDeletingPosting = () => {
       return (
         <section className="delete-section">
-          <div className="posting">
+        return (
+						<div className="posting">
+              <div className={`company-logo ${posting.company}-logo`}></div>
+							<div className="posting-block">
 								<h3 className="job-title">{posting.details.title}</h3>
-								<div className="field">
-									<span className="field-name">Company: </span>
-									<span className="job-company">{posting.company}</span>
-								</div>
-								<div className="field">
-									<span className="field-name">Job Location: </span>
-									<span className="job-location">{posting.details.location}</span>
-								</div>
-								<div className="field">
-									<span className="field-name">Profession category: </span>
-									<span className="job-location">{posting.details.profession}</span>
-								</div>
-								<div className="field">
-									<span className="field-name">Salary: </span>
-									<span className="job-salary">{posting.details.salary}</span>
-								</div>
-								<div className="hidden-block">
+                <div className="posting__info-row">
+                    <span className="field job-company">{posting.company}</span>
+                    <span className="field job-location">{posting.details.location}</span>
+                    <span className="field job-location">{posting.details.profession}</span>
+                    <span className="field job-salary">{posting.details.salary}</span>
+                </div>
+								<div  className="hidden-block visible">
 									<div className="job-description">
-										<div className="field">
-											<h4 className="field-name">Job description: </h4>
-											{posting.details.description}
-										</div>
-										<h4 className="field-name">Skills must to have</h4>
-										<ul className="field job-skills-must">
+                    {posting.details.description}
+										
+										<h4 >Skills must to have</h4>
+										<ul className="job-skills-must">
 												{posting.details.skillsMust.map((skill) => <li key={skill} className="skill">{skill}</li>)}
 										</ul>
 									</div>
-									<div className="field">
-										<span className="field-name">Phone Number: </span>
-										<span className="job-company">{posting.phone}</span>
-									</div>
-									<div className="field">
-										<span className="field-name">Email for rezume: </span>
-										<span className="job-company">{posting.email}</span>
+									<div className="contact-info">
+                    <h4 className="contact-us">Contact us:</h4>
+										<span className="field contact">Phone: {posting.phone}</span>
+										<span className="field contact">Email: {posting.email}</span>
 									</div>
 								</div>
 							</div>
+						</div>
 
               <input type="button" className="delete__button" value="Delete Posting" onClick={() => deletePosting()}/>
         </section>
       )
     }
     return (
-        <div>
+        <div className="container">
             <HeaderMain />
-            <h2>Deleting Posting</h2>
+            <h2 className="delete__title">Deleting this Posting?</h2>
             {isDeleted && deletedMessage()}
             {!isDeleted && printDeletingPosting()}
         </div>
